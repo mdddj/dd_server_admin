be9c044d14ee59279d8143465d6299d7eea64ada
梁典典 <413153189@qq.com>
2023-04-17 09:39:43 +0800

    第一次提交

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+// 运行时配置
+
+// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+import {RequestConfig} from "@@/plugin-request/request";
+
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: '@umijs/max' };
+}
+
+export const layout = () => {
+  return {
+    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    menu: {
+      locale: false,
+    },
+  };
+};
+
+
+export const request: RequestConfig = {
+  url: "https://itbug.shop:9443",
+  timeout: 1000,
+  errorConfig: {
+    errorHandler(){
+    },
+    errorThrower(){
+    }
+  },
+  requestInterceptors: [],
+  responseInterceptors: []
+};
